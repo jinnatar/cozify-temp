4f6c3f93a201383c7158302fc5890eaf491891da
Artanicus <artanicus@nocturnal.fi>
2017-02-12 16:38:09 +0200

    Attempt at better error tolerance from influxdb

--- cozify-sample-loop.py
+++ cozify-sample-loop.py
@@ -3,8 +3,15 @@ import time
 
 from cozify import cloud, hub, multisensor, storage
 
+sensors = []
 while True:
     data = hub.getDevices()
     sensors = multisensor.getMultisensorData(data)
-    storage.storeMultisensor(sensors)
+    try:
+        storage.storeMultisensor(sensors)
+    except:
+        print('Issues writing to InfluxDB')
+    else:
+        # write succeeded, drop cache
+        sensors = []
     time.sleep(5)
