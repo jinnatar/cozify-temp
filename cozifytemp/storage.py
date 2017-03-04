bc6542b6eda041fbc9739e3fe854d9bcb79c4c62
Artanicus <artanicus@nocturnal.fi>
2017-03-04 23:32:44 +0200

    Pick and store time

--- cozifytemp/storage.py
+++ cozifytemp/storage.py
@@ -11,17 +11,18 @@ db = InfluxDBClient(
         c.config['Storage']['db']
 )
 
+# helper class for defining the type of series data stored in influxDB
 class MultisensorSeries(SeriesHelper):
     class Meta:
         client = db
         series_name = 'multisensor'
-        fields = ['temperature', 'humidity']
+        fields = ['temperature', 'humidity', 'time']
         tags = ['name']
 
 
 # expects list of maps: [{name: 'foo', temperature: 42, humidity: 30}, ...]
 def storeMultisensor(sensors):
     for sensor in sensors:
-        MultisensorSeries(name=sensor['name'], temperature=sensor['temperature'], humidity=sensor['humidity'])
+        MultisensorSeries(name=sensor['name'], temperature=sensor['temperature'], humidity=sensor['humidity'], time=sensor['time'])
         print('%s, %s C, %s %%H' %(sensor['name'], sensor['temperature'], sensor['humidity']))
     MultisensorSeries.commit()
