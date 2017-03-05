7352e789b4c8e9338f8835259c21b96295ad28e3
Artanicus <artanicus@nocturnal.fi>
2017-03-05 10:05:30 +0200

    Fix multiple logic errors and document sample better
    - cache logic didn't work due to wrong method of adding to it
    - except clause was overly broad and not very descriptive
    - add more useful prints to see cache and write sample sizes

--- cozify-sample-loop.py
+++ cozify-sample-loop.py
@@ -4,15 +4,30 @@ import time
 from cozify import hub, multisensor
 from cozifytemp import storage
 
+from influxdb.exceptions import InfluxDBServerError
+
 sensors = []
+
+# loop until interrupted
 while True:
+    # cozify.hub.getDevices will return the raw devices API call blob as a dictionary
+    # it will also trigger cozify.cloud.authentication() if we don't have a valid hub key yet.
+    # if auth fails it will throw an exception and kill this loop
     data = hub.getDevices()
-    sensors = multisensor.getMultisensorData(data)
+    # cozify.multisensor.getMultisensorData will take the raw device blob and
+    # extract a list of dictionaries [{temp, humidity, time}, ...] of all the multisensor devices
+    # Data is extended into the list so that any previous cache is preserved and these are just added as new samples
+    sensors.extend(multisensor.getMultisensorData(data))
+
+    # InfluxDB writes can fail if for example IO conditions are bad
+    # to mitigate this, we'll cache results and try again on the next loop with both old & new data
     try:
+        print('writing to InfluxDB...')
         storage.storeMultisensor(sensors)
-    except:
-        print('Issues writing to InfluxDB')
+    except InfluxDBServerError(e):
+        print('Data kept in cache(%s), issues writing to InfluxDB: %s' % (len(sensors), e))
     else:
         # write succeeded, drop cache
+        print('write(%s) successful!' % len(sensors))
         sensors = []
-    time.sleep(5)
+    time.sleep(60)
