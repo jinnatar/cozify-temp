6e51c9afceed840e4a05184d82b0c3b6b2c68668
Artanicus <artanicus@nocturnal.fi>
2017-04-04 17:56:04 +0300

    Rewrite config module to use XDG paths and dump out sane defaults

--- cozifytemp/config.py
+++ cozifytemp/config.py
@@ -1,4 +1,51 @@
 import configparser
+import os
 
-config = configparser.ConfigParser()
-config.read('main.cfg')
+def _initXDG():
+    # per the XDG basedir-spec we adhere to $XDG_CONFIG_HOME if it's set, otherwise assume $HOME/.config
+    xdg_config_home = ''
+    if 'XDG_CONFIG_HOME' in os.environ:
+        xdg_config_home = os.environ['XDG_CONFIG_HOME']
+    else:
+        xdg_config_home = "%s/.config" % os.path.expanduser('~')
+
+    # XDG base-dir: "If, when attempting to write a file, the destination directory is non-existant an attempt should be made to create it with permission 0700. If the destination directory exists already the permissions should not be changed."
+    if not os.path.isdir(xdg_config_home):
+        os.mkdir(xdg_config_home, 0o0700)
+
+    # finally create our own config dir
+    config_dir = "%s/%s" % (xdg_config_home, 'cozify-temp')
+    if not os.path.isdir(config_dir):
+        os.mkdir(config_dir, 0o0700)
+
+    config_file = "%s/influxdb.cfg" % config_dir
+    return config_file
+
+def _initState(config_file):
+    # set defaults, these are used if the config doesn't override
+    config = configparser.ConfigParser()
+    config.read_dict({'Storage':
+        {
+            'host': 'localhost',
+            'port': 8086,
+            'user': '',
+            'password': '',
+            'db': 'cozify'
+        }
+    })
+
+    try:
+        cf = open(config_file, 'r')
+    except IOError: # if open fails, let's try to write one
+        cf = open(config_file, 'w+') # if this fails, let it burn
+        os.chmod(config_file, 0o600)
+        # now we should have a blank config_file so let's write out the defaults
+        print(*config, sep='\n')
+        config.write(cf)
+    else: # file is readable so let's read it in and override defaults
+        config.read_file(cf)
+
+    return config
+
+config_file = _initXDG()
+config = _initState(config_file)
