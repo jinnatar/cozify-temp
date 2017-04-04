9b11072e5148ccd0c0f7441589014cd3efd980c5
Artanicus <artanicus@nocturnal.fi>
2017-04-04 18:00:18 +0300

    Produce more helpful errors if influxdb isn't set up as defaults expect

--- cozify-single-sample.py
+++ cozify-single-sample.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 from cozify import hub, multisensor
-from cozifytemp import storage
+from cozifytemp import storage, config
+from influxdb.exceptions import InfluxDBServerError
 
 # very naive example pulling a single sample and storing it
 def main():
     data = hub.getDevices()
     sensors = multisensor.getMultisensorData(data)
-    storage.storeMultisensor(sensors)
+    try:
+        storage.storeMultisensor(sensors)
+    except:
+        print('Storage call failed, you may want to edit the db config at: %s' % config.config_file)
+        raise
+    else:
+         print('write(%s) successful!' % len(sensors))
 
 if __name__ == "__main__":
     main()
