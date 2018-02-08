4fbe4c3272477341d17e9f84d9736cb80c64b190
Artanicus <artanicus@nocturnal.fi>
2018-02-08 20:21:19 +0200

    Move conversion out of single-sample into an util module

--- cozify-single-sample.py
+++ cozify-single-sample.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from cozify import hub
-from cozifytemp import storage, config
+from cozifytemp import storage, config, util
 from influxdb.exceptions import InfluxDBServerError
 import pytz, time
 
@@ -11,31 +11,12 @@ def main():
     sensors = hub.devices(capabilities=[hub.capability.TEMPERATURE, hub.capability.HUMIDITY])
     tz=pytz.timezone(hub.tz())
     try:
-        storage.storeMultisensor(conv(sensors), tz=tz)
+        storage.storeMultisensor(util.homogenize(sensors), tz=tz)
     except:
         print('Storage call failed, you may want to edit the db config at: %s' % config.config_file)
         raise
     else:
          print('write(%s) successful!' % len(sensors))
-
-# Data smasher to fill in the gaps of what we didn't get from the devices
-def conv(sensors):
-    out = {}
-    for device_id, device in sensors.items():
-        if device_id not in out:
-            out[device_id] = {}
-        out[device_id]['name'] = device['name']
-
-        for key, default in {
-                'temperature': None,
-                'humidity': None,
-                'lastSeen': int(time.time()*1000)
-                }.items():
-            if key in device['state']:
-                out[device_id][key] = device['state'][key]
-            else:
-                out[device_id][key] = default
-    return list(out.values())
 
 if __name__ == "__main__":
     main()
---
+++ cozifytemp/util.py
@@ -0,0 +1,21 @@
+import time
+
+# Homogenize sensor data to contain all fields expected.
+# Fill in default values where value isn't available.
+def homogenize(sensors):
+    out = {}
+    for device_id, device in sensors.items():
+        if device_id not in out:
+            out[device_id] = {}
+        out[device_id]['name'] = device['name']
+
+        for key, default in {
+                'temperature': None,
+                'humidity': None,
+                'lastSeen': int(time.time()*1000)
+                }.items():
+            if key in device['state']:
+                out[device_id][key] = device['state'][key]
+            else:
+                out[device_id][key] = default
+    return list(out.values())
