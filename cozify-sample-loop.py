0a41746b465f59cf942a4dffa969dc74354cbe04
Artanicus <artanicus@nocturnal.fi>
2018-02-08 20:42:16 +0200

    Modernize the sample looper to use capabilities. Compatible with python-cozify >= v0.2.11

--- cozify-sample-loop.py
+++ cozify-sample-loop.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import time, logging, signal, sys
 
-from cozify import hub, cloud, multisensor
-from cozifytemp import storage, cache
+from cozify import hub, cloud
+from cozifytemp import storage, cache, util
 
 from influxdb.exceptions import InfluxDBServerError
 from cozify.Error import APIError
@@ -15,19 +15,19 @@ def main():
         # it will also trigger cozify.cloud.authentication() if we don't have a valid hub key yet.
         # if auth fails it will throw an APIError exception and kill this loop, so we need to check for that
         try:
+            # Check connectivity and have it auto-renewed if it's deemed time to do so.
             cloud.ping()
-            data = hub.getDevices()
+            # Get all devices that have a temperature OR humidity capability.
+            # Homogenize it to not have holes in the data.
+            data = util.homogenize(hub.devices(capabilities=[hub.capability.TEMPERATURE, hub.capability.HUMIDITY]))
         except APIError as e:
             if e.status_code == 401: # auth failed
-                logging.warning('Auth expired, attempting transparent renew')
-                cloud.authenticate(trustHub=False) # we shortcircuit some tests by already telling it to renew hub
+                logging.warning('Auth failed, this should not happen.')
             else:
                 raise # we ignore all other APIErrors and let it burn to the ground
         else: # data retrieval succeeded
-            # cozify.multisensor.getMultisensorData will take the raw device blob and
-            # extract a list of dictionaries [{temp, humidity, time}, ...] of all the multisensor devices
             # Data is extended into the list so that any previous cache is preserved and these are just added as new samples
-            sensors.extend(multisensor.getMultisensorData(data))
+            sensors.extend(data)
 
         # attempt storage if we have any to store
         if len(sensors) > 0:
@@ -35,7 +35,7 @@ def main():
             # to mitigate this, we'll cache results and try again on the next loop with both old & new data
             try:
                 print('writing to InfluxDB...')
-                storage.storeMultisensor(sensors, verbose=False)
+                storage.storeMultisensor(sensors, verbose=False) # disable verbose output to make the script more daemonizable.
             except InfluxDBServerError:
                 print('Data kept in cache(%s), issues writing to InfluxDB' % (len(sensors)))
             else:
@@ -45,6 +45,7 @@ def main():
                 cache.clear()
         time.sleep(60)
 
+# Handle cache dumping if killed.
 def sigterm_handler(signal, frame):
     global sensors
     if len(sensors) > 0:
