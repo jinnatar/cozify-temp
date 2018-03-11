edf6cfcadae1f7eff4cc93fed45224bdf16f580f
Artanicus <artanicus@nocturnal.fi>
2018-03-11 11:26:57 +0200

    Keep field types as floats, should fix issues with older influxdb
    Old versions of InfluxDB would implicitly declare a field an int if the
    first value happened to look like an int. New versions assume all
    numbers are floats but old ones don't.

--- cozifytemp/util.py
+++ cozifytemp/util.py
@@ -15,7 +15,8 @@ def homogenize(sensors):
                 'lastSeen': int(time.time()*1000)
                 }.items():
             if key in device['state']:
-                out[device_id][key] = device['state'][key]
+                # Also make sure values are floats so field type is maintained
+                out[device_id][key] = float(device['state'][key])
             else:
                 out[device_id][key] = default
     return list(out.values())
