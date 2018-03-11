499e5caaeb7b81367c0e05668461d041b8f93da0
Artanicus <artanicus@nocturnal.fi>
2018-03-11 12:14:51 +0200

    Finalize the changes to make sure types are consistent. This breaks compatibility to previous data format in InfluxDB if your humidity field has been typed as int instead of float. The issue will fix itself in a couple of days when the shard expires and the next one starts.
    For the exact time it will change:
    influx -execute "SHOW SHARDS"
    .. and check the last shards end_time
    
    See for technical details of why it needs to happen like this:
    https://docs.influxdata.com/influxdb/v1.5/troubleshooting/frequently-asked-questions/#can-i-change-a-field-s-data-type

--- cozifytemp/util.py
+++ cozifytemp/util.py
@@ -9,14 +9,17 @@ def homogenize(sensors):
             out[device_id] = {}
         out[device_id]['name'] = device['name']
 
-        for key, default in {
-                'temperature': None,
-                'humidity': None,
-                'lastSeen': int(time.time()*1000)
-                }.items():
-            if key in device['state']:
-                # Also make sure values are floats so field type is maintained
-                out[device_id][key] = float(device['state'][key])
+        for field in [
+                { 'key': 'temperature', 'default': None, 'type': float },
+                { 'key': 'humidity', 'default': None, 'type': float },
+                { 'key': 'lastSeen', 'default': int(time.time()*1000), 'type': int }
+                ]:
+            key = field['key']
+            default = field['default']
+            type_cast = field['type']
+            if key in device['state'] and device['state'][key] is not None:
+                # Also make sure values conform to the right types
+                out[device_id][key] = type_cast(device['state'][key])
             else:
                 out[device_id][key] = default
     return list(out.values())
